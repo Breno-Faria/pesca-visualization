d82ffe8f3d4f637331763cf2466addefca8a31d8
Brenofaria <brenofariac@gmail.com>
2022-06-14 22:35:00 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, url_for, redirect, request
+import psycopg2
+import os
+
+app = Flask(__name__)
+
+def connect_to_db():
+    connection = psycopg2.connect(
+        host=os.environ['host'],
+        database=os.environ['database'],
+        port=os.environ['port'],
+        user=os.environ['user'],
+        password=os.environ['password'],
+    )
+    return connection
+
+@app.route("/")
+def home():
+    connection = connect_to_db()
+    cursor = connection.cursor()
+    cursor.execute(
+        'SELECT * FROM contas'
+    )
+    accounts = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return render_template("index.html", accounts=accounts)
